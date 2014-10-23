d8bdca804442a96b700e693806d59dcd34b1535f
Michael Angileri <mangi@umich.edu>
2014-10-22 20:47:38 -0400

    Added Dictionaries to Model for gestures and active gesture bools

--- Model.cs
+++ Model.cs
@@ -4,31 +4,42 @@ using System.Collections.Generic;
 namespace StretchIt 
 {
     class Model {
-        private List<Gesture> gestures;
+        const string PULL_C = "Pull";
+        const string PUSH_C = "Push";
+
+        private Dictionary<string, Gesture> gestures;
         private Random random;
-        bool push = true;
-        bool pull = true;
+        private Dictionary<string, bool> active_gestures;
 
         public Model()
         {
             // Initialize members.
-            gestures = new List<Gesture>();
+            gestures = new Dictionary<string, Gesture>();
             random = new Random();
+            active_gestures = new Dictionary<string, bool>();
+
+            // Set each gesture to active (true)
+            active_gestures[PULL_C] = true;
+            active_gestures[PUSH_C] = true;
 
             // Add each gesture that was selected in the options screen
             // to the gestures List.
-            if(push) {
-                gestures.Add(new Push());
-            }
-            if(pull) {
-                gestures.Add(new Pull());
-            }
+            gestures[PULL_C] = new Pull();
+            gestures[PUSH_C] = new Push();
         }
 
         // Will return 
         public Gesture get_gesture()
         {
-            return gestures[random.Next(gestures.Count)];
+            List<string> keys = new List<string>();
+
+            foreach (KeyValuePair<string, bool> entry in active_gestures)
+            {
+                if (entry.Value)
+                    keys.Add(entry.Key);
+            }
+
+            return gestures[keys[random.Next(keys.Count)]];
         }
     }
 }
